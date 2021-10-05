f180e632ea1db1f2a7208401eaf698d3d3b9bda8
Nikolay Moiseyev <kolkz98mi@gmail.com>
2021-10-05 19:31:44 +0600

    Added hiding checkout, when keybord opens

---
+++ test hook-production/js/hideCheckout.js
@@ -0,0 +1,15 @@
+var windowHeight = window.innerHeight;
+var windowWidth = window.innerWidth;
+
+// Hides fixed footer, when keybord is opened
+function hideCheckout() {
+    document.write([
+        "<style>",
+        ".checkout { visibility: hidden; }",
+        "@media screen and (orientation: portrait) and (min-height: " + (Math.max(windowWidth, windowHeight) - 30) + "px)",
+        "{ .checkout { visibility: visible; } }",
+        "@media screen and (orientation: landscape) and (min-height: " + (Math.min(windowWidth, windowHeight) - 30) + "px)",
+        "{ .checkout { visibility: visible; } }",
+        "</style>"
+    ].join(" "));
+}
